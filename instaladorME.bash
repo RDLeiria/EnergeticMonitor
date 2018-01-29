db27d825f3204c9cce73dd38db5f609cb42734ce
leiria <rdleiria@gmail.com>
2018-01-28 23:18:19 -0200

    Update instaladorME.bash

--- instaladorME.bash
+++ instaladorME.bash
@@ -1,23 +1,14 @@
-#!/bin/bash
-#Instalador do Monitor Energetico - Trabalho de Conclusão de Curso - Ciência da Computação - UNIPAMPA - Campus rete
-#Feito por Raul Leiria (rdleiria@gmail.com)
-#2016-04-02 - Criacao
-#2016-07-28 - Atualizacao
+##!/bin/bash
+## 2016-04-02 - Created
+## 2016-07-28 - Last update
 
-##Comentarios pertinentes
 #tar -pcvzf ME.tar.gz ME
 #git add . && git commit -m "." && git push origin master
 
-#Constantes
 source ME/variablesME.inc
-
-#Lista de nodes
 NODES=`onehost list | tail -n +2 | cut -d " " -f5`
+ln -s $ARQDADOSMEB $ARQDADOSMEW
 
-#Link simbolico para ser possivel compartilhar o arquivo dados.txt entre os nodes e o frontend
-#ln -s $ARQDADOSMEB $ARQDADOSMEW
-
-#Extracao
 tar -pxvzf $ARQME -C $OPDIRNFS
 
 for NODE in `echo $NODES`; do
